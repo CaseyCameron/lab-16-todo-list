52ac3630bf3daa33b744a0cba81bc31f17862b0e
Katherine Tam <katherinemtam@gmail.com>
2021-04-15 13:08:35 -0700

    add todo page functionality

---
+++ task-utils.js
@@ -0,0 +1,28 @@
+export function renderLi(task) {
+    const li = document.createElement('li');
+    li.textContent = task.task;
+
+    li.addEventListener('click', () => {
+        completeLi(task.task);
+        renderTodo();
+    });
+
+    return li;
+}
+
+export function completeLi(todo) {
+    const user = getUser();
+    const matchingTask = user.tasks.find((item) => item.task === todo);
+    matchingTask.completed = true;
+    setUser(user);
+}
+
+export function renderTodo() {
+    const user = getUser();
+    const ul = document.querySelector('ul');
+    ul.textContent = '';
+    user.tasks.forEach(task => {
+        const li = renderLi(task);
+        ul.append(li);
+    });
+}
--- todo/todo.js
+++ todo/todo.js
@@ -1,4 +1,5 @@
 import { getUser, addTask, setUser } from '../local-storage-utils.js';
+import { renderTodo }
 
 const user = getUser();
 if (!user) {
@@ -10,51 +11,13 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const taskData = new FormData(form);
     const task = taskData.get('new-task');
     addTask(task);
-    const ul = document.querySelector('ul');
-    ul.textContent = '';
-    const user = getUser();
-    user.tasks.forEach(task => {
+    renderTodo();
 
-
-
-        ul.append(li);
-    });
     form.reset();
+});
 
-}
-);
-
-function renderLi(task) {
-    const li = document.createElement('li');
-    li.textContent = task.task;
-
-    li.addEventListener('click', () => {
-        completeLi(task.task);
-        renderTodo();
-    });
-
-    return li;
-}
-
-function completeLi(todo) {
-    const user = getUser();
-    const matchingTask = user.tasks.find((item) => item.task === todo);
-
-
-    matchingTask.completed = true;
-    setUser(user);
-}
-
-function renderTodo() {
-    const user = getUser();
-    const ul = document.querySelector('ul');
-    ul.textContent = '';
-    user.tasks.forEach(task => {
-        const li = renderLi(task);
-        ul.append(li);
-    });
-}
 
